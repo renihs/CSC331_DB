eb68730179a36e9e3a615b262aa5cb5e5c0a8eb6
queenofthehobos <sarah.ellie.flowers@gmail.com>
2014-11-01 14:49:41 -0500

    Create IndexFile.h

---
+++ IndexFile.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+#include <stdlib.h>
+#include <cstring>
+
+class IndexFile{
+    public:
+	IndexFile();
+	void initialBuild(std::string);
+	std::map<int,int> loadList(std::string);
+	void write(std::map<int,int>);
+	void search(const char* strKey);
+};
